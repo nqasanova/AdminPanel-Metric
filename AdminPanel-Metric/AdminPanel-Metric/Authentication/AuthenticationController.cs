07dfd01f7aed28d19c5599d25f2a71d58e0de7ba
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-11-13 22:10:35 +0400

    Authentication Controller added to the system.

---
+++ AdminPanel-Metric/AdminPanel-Metric/Authentication/AuthenticationController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel_Metric.Authentication
+{
+    public class AuthenticationController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult LoginAlt()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterAlt()
+        {
+            return View();
+        }
+
+        public ActionResult RePassword()
+        {
+            return View();
+        }
+
+        public ActionResult RePasswordAlt()
+        {
+            return View();
+        }
+
+        public ActionResult LockScreen()
+        {
+            return View();
+        }
+
+        public ActionResult LockScreenAlt()
+        {
+            return View();
+        }
+
+        public ActionResult Error404()
+        {
+            return View();
+        }
+
+        public ActionResult Error404Alt()
+        {
+            return View();
+        }
+
+        public ActionResult Error500()
+        {
+            return View();
+        }
+
+        public ActionResult Error500Alt()
+        {
+            return View();
+        }
+    }
+}
