3bec285abbc7baede4810dbfcb70596851fa95f7
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-11-13 21:10:12 +0400

    Initial structure completed.

---
+++ AdminPanel-Metric/AdminPanel-Metric/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AdminPanel_Metric;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
