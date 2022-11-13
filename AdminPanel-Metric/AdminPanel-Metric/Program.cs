b43ebe7c5e34c88b96ea12293d3f9df693432d04
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-11-13 23:07:58 +0400

    Main Program modified.

--- AdminPanel-Metric/AdminPanel-Metric/Program.cs
+++ AdminPanel-Metric/AdminPanel-Metric/Program.cs
@@ -5,9 +5,15 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddMvc();
+
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.UseStaticFiles();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Analytics}/{action=Index}");
 
         app.Run();
     }
